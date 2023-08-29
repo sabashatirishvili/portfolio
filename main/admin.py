7843e593f04aee6c7e7ae30167666c80c5d4738d
sabashatirishvili <shatirishvilis1@gmail.com>
2023-08-29 18:20:32 +0400

    Progress on Templates, Models altered

--- main/admin.py
+++ main/admin.py
@@ -3,11 +3,11 @@ from .models import *
 
 
 class ProjectAdmin(admin.ModelAdmin):
-  list_display = ('id', 'title', 'logo_url')
+  list_display = ('id', 'title', 'image_static_url')
   search_fields = ('title',)
 
 class TechnologyAdmin(admin.ModelAdmin):
-  list_display = ('id', 'name', 'logo_url')
+  list_display = ('id', 'name')
   search_fields = ('name',)
 
 # Register your models here.
--- main/models.py
+++ main/models.py
@@ -2,11 +2,22 @@ from django.db import models
 
 # Create your models here.
 class Technology(models.Model):
+  THREE_CATEGORIES = (
+    ('frontend', "Front-end"),
+    ('backend', "Back-end"),
+    ('misc', "Miscellaneous"),
+  )
+  
   name = models.CharField(max_length=25)
-  logo_url = models.FilePathField(path='media/technology_logos/')
+  category = models.CharField(
+    max_length=15,
+    choices = THREE_CATEGORIES,
+    default= 'frontend')
+  
 
 class Project(models.Model):
-  title = models.CharField(max_length=30)
-  description = models.TextField(max_length=90)
-  logo_url = models.FilePathField(path='media/project_logos/')
-  technologies = models.ManyToManyField(Technology)
+  title = models.CharField(max_length=40)
+  description = models.TextField(max_length=200)
+  image_static_url = models.CharField(max_length=225, null=True)
+  github_link = models.URLField(max_length=100)
+  website_link = models.URLField(max_length=100)
